cafc2ece3d99c7e1f52994e82f85881dc26c9693
JagerMidnight <therealjager@hotmail.com>
2025-06-03 18:57:24 -0500

    Create dungeons.js

---
+++ public/pages/war-engine/dungeons.js
@@ -0,0 +1,5 @@
+// dungeons.js
+
+export const dungeonData = [
+    { id: 'deadmines', title: 'The Deadmines', category: 'Classic', link: '#deadmines-guide' },
+    { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' },
