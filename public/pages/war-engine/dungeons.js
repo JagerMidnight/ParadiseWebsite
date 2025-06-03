0bd1f52affefbad6da8d8a83712464c54fcba52f
JagerMidnight <therealjager@hotmail.com>
2025-06-03 18:57:37 -0500

    Update dungeons.js

--- public/pages/war-engine/dungeons.js
+++ public/pages/war-engine/dungeons.js
@@ -2,4 +2,5 @@
 
 export const dungeonData = [
     { id: 'deadmines', title: 'The Deadmines', category: 'Classic', link: '#deadmines-guide' },
-    { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' },
+    { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' }
+]}
