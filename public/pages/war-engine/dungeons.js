f0ff3382e9cd9e8429d26eefd945e5d1e0253462
JagerMidnight <therealjager@hotmail.com>
2025-06-03 20:51:28 -0500

    Update dungeons.js

--- public/pages/war-engine/dungeons.js
+++ public/pages/war-engine/dungeons.js
@@ -3,4 +3,4 @@
 export const dungeonData = [
     { id: 'ragefire', title: 'Ragefire Chasm', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/ragefire-chasm-dungeon-guide' },
     { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' }
-]}
+]
