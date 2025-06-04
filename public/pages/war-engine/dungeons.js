6c3994b6058041c4bbc4ca45ba60c83252daa8d2
JagerMidnight <therealjager@hotmail.com>
2025-06-03 20:55:31 -0500

    Update dungeons.js

--- public/pages/war-engine/dungeons.js
+++ public/pages/war-engine/dungeons.js
@@ -2,5 +2,8 @@
 
 export const dungeonData = [
     { id: 'ragefire', title: 'Ragefire Chasm', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/ragefire-chasm-dungeon-guide' },
+    { id: 'deadmines', title: 'Deadmines', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/deadmines-dungeon-guide' },
+    { id: 'wailing-caverns', title: 'Wailing Caverns', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/wailing-caverns-dungeon-guide' },
+    { id: 'shadowfang', title: 'Shadowfang Keep', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/shadowfang-keep-dungeon-guide' },
     { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' }
 ]
