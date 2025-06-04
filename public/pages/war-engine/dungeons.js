df947ad50e7123ea9fb69a7f3ba522db990a6a5a
JagerMidnight <therealjager@hotmail.com>
2025-06-03 20:57:12 -0500

    Update dungeons.js

--- public/pages/war-engine/dungeons.js
+++ public/pages/war-engine/dungeons.js
@@ -5,5 +5,18 @@ export const dungeonData = [
     { id: 'deadmines', title: 'Deadmines', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/deadmines-dungeon-guide' },
     { id: 'wailing-caverns', title: 'Wailing Caverns', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/wailing-caverns-dungeon-guide' },
     { id: 'shadowfang', title: 'Shadowfang Keep', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/shadowfang-keep-dungeon-guide' },
-    { id: 'stockades', title: 'The Stockades', category: 'Classic', link: '#stockades-guide' }
-]
+    { id: 'blackfathom', title: 'Blackfathom Deeps', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/blackfathom-deeps-dungeon-guide' },
+    { id: 'stockades', title: 'The Stockades', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/the-stockade-dungeon-guide' },
+    { id: 'razorfen-kraul', title: 'Razorfen Kraul', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/razorfen-kraul-dungeon-guide' },
+    { id: 'gnomeregan', title: 'Gnomeregan', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/gnomeregan-dungeon-guide' },
+    { id: 'sm-graveyard', title: 'Scarlet Monastery Graveyard', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/scarlet-monastery-graveyard-dungeon-guide' },
+    { id: 'sm-library', title: 'Scarlet Monastery Library', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/scarlet-monastery-library-dungeon-guide' },
+    { id: 'sm-armory', title: 'Scarlet Monastery Armory', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/scarlet-monastery-armory-dungeon-guide' },
+    { id: 'sm-cathedral', title: 'Scarlet Monastery Cathedral', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/scarlet-monastery-cathedral-dungeon-guide' },
+    { id: 'razorfen-downs', title: 'Razorfen Downs', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/razorfen-downs-dungeon-guide' },
+    { id: 'uldaman', title: 'Uldaman', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/uldaman-dungeon-guide' },
+    { id: 'zulfarrak', title: 'Zul\'Farrak', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/zul-farrak-dungeon-guide' },
+    { id: 'maraudon', title: 'Maraudon', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/maraudon-dungeon-guides-hub' },
+    { id: 'brd', title: 'Blackrock Depths', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/blackrock-depths-dungeon-guides-hub' },
+    { id: 'sunken-temple', title: 'Sunken Temple', category: 'Classic', link: 'https://www.icy-veins.com/wow-classic/sunken-temple-dungeon-guide' }
+];
